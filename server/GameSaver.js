2517aea3c2ded3e3ec076aeebfddfa9dffac94d7
Rahmet23 <Rahmet23@bitbucket.org>
2024-10-08 10:21:05 +0200

    Gamesaver branch

---
+++ server/GameSaver.js
@@ -0,0 +1,16 @@
+//import java.sql.Connection;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+const mysql = require('mysql');
+
+// MySQL Connection
+const connection = mysql.createConnection(
+    {host: "localhost",
+        user: "root",
+        password: "",
+        database: "thebestseller"});
+
+// Hier komt het Game server code pion positie, speleraantal, username, strategie, roomcode, leaderboard/punten, playerturn,taal zie data.json Het moet een Json bestand worden
+void class GameSaver {
+
+}
